eee1546df51f5bc7fc2c8d18e82c39d51c85effb
DevExpressExampleBot <fakeEmail>
2022-12-17 14:06:11 +0400

    Source auto update [skip ci]

---
+++ VB/DockLayoutControl_Ex/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace DockLayoutControl_Ex
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
